f8b22417cc4749905f4b85316af68e9d9d68bc90
giau phan <giauphan012@gmail.com>
2023-07-26 10:04:39 +0700

    dele auth

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/api/process-file', [FileController::class, 'index']);
-    Route::post('/api/process-file', [FileController::class, 'process']);
-});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

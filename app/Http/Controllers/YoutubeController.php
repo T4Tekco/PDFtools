d110296ee950f6d34b936c033158b49fe59a3137
giauphan <giauphan012@gmail.com>
2023-12-25 16:21:56 +0700

    update title video

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -3,38 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
-use YoutubeDl\Options;
-use YoutubeDl\YoutubeDl;
 
 class YoutubeController extends Controller
 {
     public function getVideo(Request $request)
     {
-        ini_set('max_execution_time', 300);
 
         if ($request->has('url')) {
             $videoUrl = $request->input('url');
 
-            $yt = new YoutubeDl();
-            $downloadPath = public_path('/storage');
+            $command = 'youtube-dl --verbose -g -e ' . $videoUrl;
+            // Execute the command
+            $output = shell_exec($command);
 
-            $collection = $yt->download(
-                Options::create()
-                    ->downloadPath($downloadPath)
-                    ->format('mp4')
-                    ->url($videoUrl)
-            );
-            foreach ($collection->getVideos() as $video) {
-                if ($video->getError() !== null) {
-                    return response()->json(['error' => "Error downloading video"]);
-                } else {
-                    $file = $video->getFile();
-                    $fileContent = base64_encode(file_get_contents($file->getPathname()));
-                    return response()->json([
-                        'file_content' => $fileContent
-                    ]);
-                }
+            // Split the output into an array of URLs
+            $urls = explode(PHP_EOL, trim($output));
+
+            // Now $urls should contain two URLs: video and audio
+            if (count($urls)) {
+                $title = $urls[0];
+                $videoUrl = $urls[1];
+                $audioUrl = $urls[2];
+
+                return response()->json([
+                    'title' =>   $title,
+                    "url_video" => $videoUrl,
+                    'url_audio' => $audioUrl
+                ]);
+            } else {
+                return response()->json(['error', "Error retrieving video and audio URLs."]);
             }
         }
 
